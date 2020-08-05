3164ff1bdcfa26f678db16c996cf2607f734951a
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-04 22:56:17 -0300

    Mover tela principal para pasta Views

--- TikTokClone/App.xaml.cs
+++ TikTokClone/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TikTokClone.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
--- TikTokClone/Views/MainPage.xaml.cs
+++ TikTokClone/Views/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace TikTokClone
+namespace TikTokClone.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
