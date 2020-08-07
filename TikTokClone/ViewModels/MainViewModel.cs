1543a7c34809d8d9ad63468dae73e170c38d0dd8
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-06 21:23:46 -0300

    Remover comandos desnecessários

--- TikTokClone/ViewModels/MainViewModel.cs
+++ TikTokClone/ViewModels/MainViewModel.cs
@@ -18,14 +18,6 @@ namespace TikTokClone.ViewModels
         }
 
         public List<TabItem> Tabs { get; private set; }
-        public ICommand PositionChangedCommand { get; private set; }
-
-        private Xamarin.Forms.Color _tabBackground;
-        public Xamarin.Forms.Color TabBackground
-        {
-            get => _tabBackground;
-            set => SetProperty(ref _tabBackground, value);
-        }
 
         public MainViewModel()
         {
@@ -37,24 +29,6 @@ namespace TikTokClone.ViewModels
                 new TabItem("Inbox", typeof(TabItemInboxView)),
                 new TabItem("Me", typeof(TabItemMeView))
             };
-
-            TabBackground = Xamarin.Forms.Color.Transparent;
-
-            PositionChangedCommand = new Command(PositionChanged);
-        }
-
-        private void PositionChanged()
-        {
-            if (CurrentTab is null)
-                return;
-
-            if (CurrentTab.DataTemplateType == typeof(TabItemHomeView))
-            {
-                TabBackground = Xamarin.Forms.Color.Transparent;
-                return;
-            }
-
-            TabBackground = Xamarin.Forms.Color.Yellow;
         }
     }
 }
