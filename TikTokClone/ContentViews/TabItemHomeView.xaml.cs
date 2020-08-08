6555fd5a63ee5266b5a1ceecb7ed5aeee05f233d
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-07 23:23:29 -0300

    Parar vídeo que está saindo da tela ao fazer scroll

--- TikTokClone/ContentViews/TabItemHomeView.xaml.cs
+++ TikTokClone/ContentViews/TabItemHomeView.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using System.Collections.Generic;
 using TikTokClone.ViewModels;
 using Xamarin.Forms;
@@ -15,6 +15,17 @@ namespace TikTokClone.ContentViews
 
         private void OnPositionChanged(object sender, PositionChangedEventArgs args)
         {
+            if (IsScrollUp(args))
+            {
+                if (CarouselViewVideos.VisibleViews.FirstOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoUp)
+                    videoUp.Stop();
+
+                if (CarouselViewVideos.VisibleViews.LastOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoDown)
+                    videoDown.Play();
+            }
         }
+
+        private bool IsScrollUp(PositionChangedEventArgs args) => args.PreviousPosition < args.CurrentPosition;
+        private bool IsScrollDown(PositionChangedEventArgs args) => !IsScrollUp(args);
     }
 }
