b32a439c613bd75793a7c331c19434bdf003158a
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-07 23:38:50 -0300

    Simplifica o controle do vídeo
    Os itens visíveis não alteram a ordem se o scroll é para cima ou para baixo, então, o desenvolvimento anterior foi desnecessário.

--- TikTokClone/ContentViews/TabItemHomeView.xaml.cs
+++ TikTokClone/ContentViews/TabItemHomeView.xaml.cs
@@ -15,30 +15,20 @@ namespace TikTokClone.ContentViews
 
         private void OnPositionChanged(object sender, PositionChangedEventArgs args)
         {
-            if (IsScrollUp(args))
-            {
-                if (CarouselViewVideos.VisibleViews.FirstOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoUp)
-                    videoUp.Stop();
-
-                if (CarouselViewVideos.VisibleViews.LastOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoDown)
-                    videoDown.Play();
-
-                return;
-            }
-
-            if (IsScrollDown(args))
-            {
-                if (CarouselViewVideos.VisibleViews.FirstOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoUp)
-                    videoUp.Stop();
-
-                if (CarouselViewVideos.VisibleViews.LastOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoDown)
-                    videoDown.Play();
-
-                return;
-            }
+            StopVideoOutOfBounds();
+            PlayVideoInOfBounds();
         }
 
-        private bool IsScrollUp(PositionChangedEventArgs args) => args.PreviousPosition < args.CurrentPosition;
-        private bool IsScrollDown(PositionChangedEventArgs args) => !IsScrollUp(args);
+        private void StopVideoOutOfBounds()
+        {
+            if (CarouselViewVideos.VisibleViews.FirstOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoOutOfBounds)
+                videoOutOfBounds.Stop();
+        }
+
+        private void PlayVideoInOfBounds()
+        {
+            if (CarouselViewVideos.VisibleViews.LastOrDefault()?.FindByName<MediaElement>("Video") is MediaElement videoInOfBounds)
+                videoInOfBounds.Play();
+        }
     }
 }
