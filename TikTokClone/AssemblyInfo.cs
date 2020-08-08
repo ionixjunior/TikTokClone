6fa16381170c97e46b691e9cd941504c4d643143
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-08 12:25:13 -0300

    Registrar fonte semi bold

--- TikTokClone/AssemblyInfo.cs
+++ TikTokClone/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Proxima-Nova-Regular.woff2", Alias = "ProximaRegular")]
+[assembly: ExportFont("Proxima-Nova-Semibold.woff2", Alias = "ProximaSemiBold")]
