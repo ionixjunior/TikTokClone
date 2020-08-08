e1cac41fb6a4e21ce8f573aa1a0970de89127b1a
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-08 12:14:38 -0300

    Registar fonte adicionada

--- TikTokClone/AssemblyInfo.cs
+++ TikTokClone/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Proxima-Nova-Regular.woff2", Alias = "ProximaRegular")]
