b2fd073a4262fad4e2064b890a8a33670607e454
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-09 14:04:09 -0300

    Criar bindable property para texto animado e atribuir no Text do label

--- TikTokClone/Controls/MarqueeLabel.cs
+++ TikTokClone/Controls/MarqueeLabel.cs
@@ -5,6 +5,30 @@ namespace TikTokClone.Controls
 {
     public class MarqueeLabel : Label
     {
+        public static readonly BindableProperty AnimatedTextProperty =
+            BindableProperty.Create(
+                nameof(AnimatedText),
+                typeof(string),
+                typeof(MarqueeLabel),
+                string.Empty,
+                BindingMode.OneWay,
+                propertyChanged: OnAnimatedTextChanged
+            );
+
+        public string AnimatedText
+        {
+            get { return (string)GetValue(AnimatedTextProperty); }
+            set { SetValue(AnimatedTextProperty, value); }
+        }
+
+        private static void OnAnimatedTextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            System.Diagnostics.Debug.WriteLine($"Texto alterado: {newValue}");
+
+            if (bindable is MarqueeLabel label)
+                label.Text = (string)newValue;
+        }
+
         public MarqueeLabel()
         {
         }
