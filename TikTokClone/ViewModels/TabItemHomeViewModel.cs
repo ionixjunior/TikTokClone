ea774566ac55829223bf3d01af82fd0a9dc5c44b
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-09 01:20:13 -0300

    Trocar novamente o link do vídeo

--- TikTokClone/ViewModels/TabItemHomeViewModel.cs
+++ TikTokClone/ViewModels/TabItemHomeViewModel.cs
@@ -10,7 +10,7 @@ namespace TikTokClone.ViewModels
 
         public TabItemHomeViewModel()
         {
-            var linkExemplo = "https://v16m.tiktokcdn.com/3b928120abddc4435131a19ed2f7c0e5/5f320550/video/tos/useast2a/tos-useast2a-ve-0068c004/5b8cb85c601d4619b635961255179e4b/?a=1233&br=3858&bt=1929&cr=0&cs=0&dr=0&ds=3&er=&l=2020080902412001018907409218046FE8&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajh3NXVnN2RvdDMzNjczM0ApOTM7Ozw6ZWU8NzU1Z2ZpOGczZV40cWRrbXBfLS0yMTZzc18vX14xLWA0YmE0L2NeXjA6Yw%3D%3D&vl=&vr=";
+            var linkExemplo = "https://v16m.tiktokcdn.com/fb5f45461ca8ae8e24b7cbe864ea8747/5f321a35/video/tos/useast2a/tos-useast2a-ve-0068/7fcc89a31a364ca9a651169fa1ba9d49/?a=1233&br=3108&bt=1554&cr=0&cs=0&dr=0&ds=3&er=&l=202008090410290101901760400B014510&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M3NvcDc0eWhzcDMzZjczM0ApOzo3NDQ1Nzs6NzU0NDM2O2cyaHFuNnJmbDRfLS01MTZzczZjNDVfNi02NC8xXl9jMF86Yw%3D%3D&vl=&vr=";
             Videos = new ObservableRangeCollection<Video>();
             Videos.Add(new Video("#ff0000", linkExemplo));
             Videos.Add(new Video("#00ff00", linkExemplo));
