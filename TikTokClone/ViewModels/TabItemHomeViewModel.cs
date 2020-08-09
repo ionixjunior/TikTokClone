3a3f1c819d0fb21fe30bd7b1970ddb4f0c505310
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-08 23:42:35 -0300

    Alterar vídeo de exemplo

--- TikTokClone/ViewModels/TabItemHomeViewModel.cs
+++ TikTokClone/ViewModels/TabItemHomeViewModel.cs
@@ -10,7 +10,7 @@ namespace TikTokClone.ViewModels
 
         public TabItemHomeViewModel()
         {
-            var linkExemplo = "https://v16m.tiktokcdn.com/221e2077660762507810ff7c03194793/5f2f4d61/video/tos/useast2a/tos-useast2a-pve-0068/d86080fc80ff49fea2cf12e03bafda8a/?a=1233&br=2298&bt=1149&cr=0&cs=0&dr=0&ds=3&er=&l=2020080701120001018906603211655894&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=am5mNjloZzM7dTMzOzczM0ApOzQ3OjhnZmQ4NzQ4PGQ2Omc0X2AtNmJraDJfLS01MTZzc2EuLjJeNDQtYTVhNDYwLy86Yw%3D%3D&vl=&vr=";
+            var linkExemplo = "https://v16m.tiktokcdn.com/b1faabf5380fc11321846e43f49e3390/5f320550/video/tos/alisg/tos-alisg-pve-0037/c64d7e9367ad4dcd9d53181ab9375565/?a=1180&br=2106&bt=1053&cr=0&cs=0&dr=3&ds=3&er=&l=2020080902412001018907409218046FE8&lr=tiktok&mime_type=video_mp4&qs=0&rc=ang3OXE5aWd4dTMzaTgzM0ApMzU4NWk7Nzs6Nzo2PDtnZWdqYTJrY2NrMjJfLS0zLzRzczVfXjAzYWMxYl9hYzAzNC46Yw%3D%3D&vl=&vr=";
             Videos = new ObservableRangeCollection<Video>();
             Videos.Add(new Video("#ff0000", linkExemplo));
             Videos.Add(new Video("#00ff00", linkExemplo));
