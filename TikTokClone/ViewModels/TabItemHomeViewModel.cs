0649e41901f56c6a75249063ec37f95276e092de
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-09 02:44:02 -0300

    Alterar mais uma vez o vídeo
    É para constrastar melhor as cifras.

--- TikTokClone/ViewModels/TabItemHomeViewModel.cs
+++ TikTokClone/ViewModels/TabItemHomeViewModel.cs
@@ -10,7 +10,7 @@ namespace TikTokClone.ViewModels
 
         public TabItemHomeViewModel()
         {
-            var linkExemplo = "https://v16m.tiktokcdn.com/fb5f45461ca8ae8e24b7cbe864ea8747/5f321a35/video/tos/useast2a/tos-useast2a-ve-0068/7fcc89a31a364ca9a651169fa1ba9d49/?a=1233&br=3108&bt=1554&cr=0&cs=0&dr=0&ds=3&er=&l=202008090410290101901760400B014510&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M3NvcDc0eWhzcDMzZjczM0ApOzo3NDQ1Nzs6NzU0NDM2O2cyaHFuNnJmbDRfLS01MTZzczZjNDVfNi02NC8xXl9jMF86Yw%3D%3D&vl=&vr=";
+            var linkExemplo = "https://v16m.tiktokcdn.com/5a1c25ea2ab48981a704a0d993fc4a5f/5f322faa/video/tos/useast2a/tos-useast2a-ve-0068c004/9fa3da104133427b84799ab69672417d/?a=1233&br=1636&bt=818&cr=0&cs=0&dr=0&ds=3&er=&l=2020080905420201019019104736013EA8&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajk0ZW1nZDM8czMzNTczM0ApPGU6OjZlaDs8NzQ6NmhoNGdyaG9fbXBuZmtfLS1iMTZzczEvMTBeMTZhXmFfMGAtLzY6Yw%3D%3D&vl=&vr=";
             Videos = new ObservableRangeCollection<Video>();
             Videos.Add(new Video("#ff0000", linkExemplo));
             Videos.Add(new Video("#00ff00", linkExemplo));
